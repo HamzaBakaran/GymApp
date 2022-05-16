dca761ce884235e76f98d9fba009f2d4eb467838
HamzaBakaran <hamza.bakaran@stu.ibu.edu.ba>
2022-05-16 15:37:29 +0200

    Digital Ocean update

--- rest/Dao/BaseDao.class.php
+++ rest/Dao/BaseDao.class.php
@@ -9,12 +9,16 @@ class BaseDao{
   * constructor of dao class
   */
   public function __construct($table_name){
-    $this->table_name = $table_name;
-    $servername = "localhost:3307";
-    $username = "root";
-    $password = "13062000";
-    $schema = "gymdb";
-    $this->conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+      $this->table_name = $table_name;
+      $servername = Config::DB_HOST();
+      $username = Config::DB_USERNAME();
+      $password = Config::DB_PASSWORD();
+      $schema = Config::DB_SCHEME();
+      $port = Config::DB_PORT();
+      $this->conn = new PDO("mysql:host=$servername;dbname=$schema;port=$port", $username, $password);
+      // set the PDO error mode to exception
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
 
     if($this->conn){
       echo 'connected ';
--- rest/Dao/UserDao.class.php
+++ rest/Dao/UserDao.class.php
@@ -9,6 +9,9 @@ class UserDao extends BaseDao{
     parent::__construct("users");
   }
 
+  public function get_user_by_email($email){
+     return $this->query_unique("SELECT * FROM users WHERE email = :email", ['email' => $email]);
+   }
 
 }
 
