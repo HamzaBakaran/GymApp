b3235ff65a86b58dbdb00b33fa915cbe096e13ca
HamzaBakaran <hamza.bakaran@stu.ibu.edu.ba>
2022-05-18 10:05:07 +0200

    changes

--- rest/services/UserService.class.php
+++ rest/services/UserService.class.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__.'/BaseService.class.php';
-// require_once __DIR__.'/../Dao/UserDao.class.php';
-require_once(realpath(dirname(__FILE__) . ''/../Dao/UserDao.class.php'));
-
+ //require_once __DIR__.'/rest/Dao/UserDao.class.php';
+ require_once __DIR__.'/../Dao/UserDao.class.php';
+// C:\Bitnami\wampstack-8.1.2-0\apache2\htdocs\first\rest\Dao
 class UserService extends BaseService{
 
   public function __construct(){
