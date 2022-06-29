46b77b1c813a2e8f0f6922833c3d6d4f2909a70c
HamzaBakaran <hamza.bakaran@stu.ibu.edu.ba>
2022-06-29 08:57:16 +0200

    JquerySPApp

--- js/membership-service.js
+++ js/membership-service.js
@@ -81,6 +81,33 @@ var MembershipService = {
       }
     });
   },
+  list: function(){
+  /*  $.get("rest/users", function(data) {
+      $("#user-list").html("");
+      var html = "";
+      */
+      $.ajax({
+      url: "rest/membership",
+      type: "GET",
+      beforeSend: function(xhr){
+        xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
+      },
+      success: function(data) {
+        $("#membership-table").html("");
+        var html = "";
+      for(let i = 0; i < data.length; i++){
+        html += `
+      <th>`+data[i].id+`</th><th>`+data[i].user_id+`</th><th>`+data[i].membership.id +`</th><th>`+data[i].start_date +`</th><th>`+data[i].end_date +`</th>`
+        `;
+      }
+      $("#membership-table").html(html);
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      toastr.error(XMLHttpRequest.responseJSON.message);
+      
+    }
+    });
+  },
 
   current_date: function(){
     const today = new Date();
